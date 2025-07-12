90e6c90e4cbcec675d6315d9bac57e2a4b3e49aa
João Vitor Pagani <joaovitorpagani7@hotmail.com>
2025-07-12 16:09:52 -0300

    feat: add component highight

---
+++ landing_page_front/src/components/highlight/Highlight.jsx
@@ -0,0 +1,26 @@
+import styles from '../highlight/Highlight.module.css'
+
+import jeep from '../../assets/jeep-wrangler.svg'
+ 
+function Highlight() {
+  return (
+    <section className={styles.containerRoot}>
+      <div className={styles.container}>
+        <div>
+          <h1>Jeep Wrangler</h1>
+        </div>
+        <div className={styles.subtext}>
+          <span>Seminovo em excelente estado,</span>
+          <span>confira os detalhes abaixo.</span>
+        </div>
+      </div>
+
+      <div className={styles.containerImg}>
+        <img src={ jeep } />
+      </div>
+    </section>
+  )
+}
+
+
+export default Highlight;
--- landing_page_front/src/pages/HomePage.jsx
+++ landing_page_front/src/pages/HomePage.jsx
@@ -1,10 +1,12 @@
 import Header from "../components/header/Header";
+import Highlight from "../components/highlight/Highlight";
 
 function Home() {
   return (
-    <>
+    <main>
       <Header />
-    </>
+      <Highlight />
+    </main>
   )
 }
 
