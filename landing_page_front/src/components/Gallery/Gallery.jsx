6a0c0839f6b0edbf5ded097f3f403abeaa4ff2e4
João Vitor Pagani <joaovitorpagani7@hotmail.com>
2025-07-13 14:18:24 -0300

    feat: adicionando responsividade para a galeria e detalhes do carro

--- landing_page_front/src/components/Gallery/Gallery.jsx
+++ landing_page_front/src/components/Gallery/Gallery.jsx
@@ -1,9 +1,9 @@
 import styles from "./Gallery.module.css";
 
-function Gallery({images}) {
+function Gallery({ images }) {
   return (
     <section className={styles.gallery}>
-      <h3 className={styles.title}>Veja cada detalhe do veículo </h3>
+      <h3 className={styles.title}>Galeria de fotos do veículo.</h3>
       <div className={styles.grid}>
         {images.map((img) => (
           <div key={img.id} className={styles.card}>
