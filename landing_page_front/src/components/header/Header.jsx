6a7bd11aafbed6c1cdd337142d2787cadce2cc80
João Vitor Pagani <joaovitorpagani7@hotmail.com>
2025-07-13 13:59:32 -0300

    feat: implementa header respoivo

--- landing_page_front/src/components/header/Header.jsx
+++ landing_page_front/src/components/header/Header.jsx
@@ -1,23 +1,65 @@
 import styles from "../header/Header.module.css";
 import logo from "../../assets/Logo.svg";
+import { useState } from "react";
 
 function Header() {
+  const [menuOpen, setMenuOpen] = useState(false);
+  const [active, setActive] = useState("home");
+
+  const toggleMenu = () => {
+    setMenuOpen(!menuOpen);
+  };
+
   return (
     <header className={styles.header}>
       <div className={styles.container}>
         <div className={styles.logo}>
           <img src={logo} alt="logo" />
         </div>
-        <nav className={styles.navigation}>
+
+        <div className={styles.menuButton} onClick={toggleMenu}>
+          <span></span>
+          <span></span>
+          <span></span>
+        </div>
+
+        <nav className={`${styles.navigation} ${menuOpen ? styles.open : ""}`}>
           <ul>
             <li>
-              <a href="#sobre">Home</a>
+              <a
+                href="#sobre"
+                className={active === "home" ? styles.active : ""}
+                onClick={() => {
+                  setActive("home");
+                  setMenuOpen(false);
+                }}
+              >
+                Home
+              </a>
             </li>
             <li>
-              <a href="#contato">Contato</a>
+              <a
+                href="#contato"
+                className={active === "contato" ? styles.active : ""}
+                onClick={() => {
+                  setActive("contato");
+                  setMenuOpen(false);
+                }}
+              >
+                Contato
+              </a>
             </li>
             <li>
-              <a href="#anunciar">Sobre</a>
+              <a
+                href="#anunciar"
+                className={active === "sobre" ? styles.active : ""}
+                onClick={() => {
+                  setActive("sobre");
+                  setMenuOpen(false);
+                }}
+              >
+                Sobre
+              </a>
             </li>
           </ul>
         </nav>
